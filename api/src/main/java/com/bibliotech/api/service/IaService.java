f46e95cb1437a4b8a69a46fc09f2b0f3432496ce
Caiketadeus <caikecarioca@gmail.com>
2026-07-30 07:35:38 -0300

    import faltando

--- api/src/main/java/com/bibliotech/api/service/IaService.java
+++ api/src/main/java/com/bibliotech/api/service/IaService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 import com.fasterxml.jackson.databind.JsonNode;
